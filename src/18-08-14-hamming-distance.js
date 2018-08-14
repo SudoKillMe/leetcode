d994ed84dfb0f49a1081f7d63ff150305ae21a65
techer <348262038@qq.com>
2018-08-14 23:36:06 +0800

    ADD: hammingDistance

---
+++ src/18-08-14-hamming-distance.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+
+const hammingDistance = function (x, y) {
+  let x_binary = x.toString(2);
+  let y_binary = y.toString(2);
+  const x_len = x_binary.length;
+  const y_len = y_binary.length;
+  if (x_len < y_len) {
+    x_binary = Array(y_len - x_len).fill(0).join('') + x_binary;
+  } else if (y_len < x_len) {
+    y_binary = Array(x_len - y_len).fill(0).join('') + y_binary;
+  }
+  let result = 0;
+  for (let i = 0; i < x_binary.length; i++) {
+    if (x_binary[i] !== y_binary[i]) {
+      result += 1;
+    }
+  }
+  return result;
+}
+
+assert.equal(2, hammingDistance(1, 4));
+assert.equal(1, hammingDistance(1, 3));
+assert.equal(1, hammingDistance(1, 5));
