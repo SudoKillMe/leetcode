4d3e68008f69c0e081e2f2481311c906848b4085
techer <348262038@qq.com>
2018-08-16 23:31:16 +0800

    ADD index in mountain array

--- src/18-08-16-index-in-mountain-array.js
+++ src/18-08-16-index-in-mountain-array.js
@@ -16,3 +16,21 @@ const peakIndexInMountainArray = function (A) {
   return -1;
 }
 
+const peakIndexInMountainArray2 = function (A) {
+  if (A.length < 3 || A.length > 10000) {
+    return -1;
+  }
+
+  let start = 0;
+  let end = A.length - 1;
+
+  while (A[start] < A[start + 1] && A[end] < A[end - 1]) {
+    start++;
+    end--;
+  }
+  if (A[start] > A[start + 1]) {
+    return start;
+  } else {
+    return end;
+  }
+}
