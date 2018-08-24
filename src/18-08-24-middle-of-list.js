048ea11f246e7856d491e8d60fcba46fb8abfd02
techer <348262038@qq.com>
2018-08-24 09:12:02 +0800

    MODIFY middle of list

--- src/18-08-24-middle-of-list.js
+++ src/18-08-24-middle-of-list.js
@@ -11,4 +11,16 @@ const middleNode = function (head) {
   return result[Math.floor(result.length / 2)];
 }
 
-middleNode([1,2,3,4,5]);
+const middleNode2 = function (head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next
+    fast = fast.next.next
+  }
+
+  return slow
+}
+
+middleNode([1,2,3,4,5,6]);
