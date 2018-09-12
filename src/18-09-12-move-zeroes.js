c2e39c40b184a51074e8b14ef48ecb838a909377
techer <348262038@qq.com>
2018-09-12 09:03:08 +0800

    add move-zeroes uncomplete

---
+++ src/18-09-12-move-zeroes.js
@@ -0,0 +1,29 @@
+
+const moveZeroes = function (nums) {
+  let index=0;
+  let lastZ = nums.length -1;
+  let len = nums.length;
+
+  while (nums[lastZ] === 0) {
+    lastZ--;
+  }
+
+  while (index < len) {
+    if (index === lastZ) {
+      break;
+    }
+    if (nums[index] === 0) {
+      swap(nums, index, lastZ);
+      lastZ--;
+    }
+    index++;
+  }
+
+  // return nums;
+}
+
+function swap (arr, i1, i2) {
+  const tmp = arr[i1];
+  arr[i1] = arr[i2];
+  arr[i2] = tmp;
+}
