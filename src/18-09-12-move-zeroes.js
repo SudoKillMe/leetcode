0cb492ee2ac596f598daa80700bf166f75d471de
techer <348262038@qq.com>
2018-09-13 21:20:19 +0800

    modify

--- src/18-09-12-move-zeroes.js
+++ src/18-09-12-move-zeroes.js
@@ -1,23 +1,23 @@
 
 const moveZeroes = function (nums) {
-  let index=0;
-  let lastZ = nums.length -1;
-  let len = nums.length;
+  // let index=0;
+  // let lastZ = nums.length -1;
+  // let len = nums.length;
 
-  while (nums[lastZ] === 0) {
-    lastZ--;
-  }
+  // while (nums[lastZ] === 0) {
+  //   lastZ--;
+  // }
 
-  while (index < len) {
-    if (index === lastZ) {
-      break;
-    }
-    if (nums[index] === 0) {
-      swap(nums, index, lastZ);
-      lastZ--;
-    }
-    index++;
-  }
+  // while (index < len) {
+  //   if (index === lastZ) {
+  //     break;
+  //   }
+  //   if (nums[index] === 0) {
+  //     swap(nums, index, lastZ);
+  //     lastZ--;
+  //   }
+  //   index++;
+  // }
 
   // return nums;
 }
