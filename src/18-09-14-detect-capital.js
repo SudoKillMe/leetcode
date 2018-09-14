92e30f43d468cbe7848a561b9f6f7b9cc369baff
techer <348262038@qq.com>
2018-09-14 21:57:55 +0800

    add detect-capital

---
+++ src/18-09-14-detect-capital.js
@@ -0,0 +1,36 @@
+
+const detectCapialUse = function (word) {
+  const arr = word.split('');
+  if (isUpper(arr[0])) {
+    const left = arr.slice(1);
+    console.log('letf: ', left);
+    return arr.every(c => {
+      return isUpper(c);
+    }) || left.every(c => {
+      return isLower(c);
+    }); 
+  } else {
+    return arr.every(c => {
+      return isLower(c);
+    });
+  }
+}
+
+function isLower (char) {
+  const ascii = char.charCodeAt();
+  return ascii >= 97 && ascii <= 122;
+}
+
+function isUpper (char) {
+  const ascii = char.charCodeAt();
+  return ascii >= 65 && ascii <= 90;
+}
+
+const test = 'USA';
+console.log(detectCapialUse(test));
+
+const test2 = 'FLaG';
+console.log(detectCapialUse(test2));
+
+const test3 = 'Google';
+console.log(detectCapialUse(test3));
