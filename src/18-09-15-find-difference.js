6cf00db541d1a75b4935e580a26c47b6b0077272
techer <348262038@qq.com>
2018-09-16 09:49:04 +0800

    find difference modify

--- src/18-09-15-find-difference.js
+++ src/18-09-15-find-difference.js
@@ -3,7 +3,6 @@ const findTheDifference = function (s, t) {
   const len = t.length;
   for (let i=0; i< len; i++) {
     const sub = t.slice(0, i) + t.slice(i + 1);
-    console.log('i: ', i, ' sub: ',sub);
     if (sub === s) {
       return t[i];
     }
@@ -11,7 +10,21 @@ const findTheDifference = function (s, t) {
   return '';
 }
 
+const findTheDifferenceModify = function (s, t) {
+  const sArr = s.split('');
+  const tArr = t.split('');
+
+  for (let i=0, len=sArr.length; i<len; i++) {
+    if (sArr[i] !== tArr[i]) {
+      return tArr[i];
+    }
+  }
+
+  return tArr[tArr.length - 1];
+}
+
+
 const s = 'abcd';
 const t = 'abcde';
 
-console.log(findTheDifference(s, t));
+console.log(findTheDifferenceModify(s, t));
